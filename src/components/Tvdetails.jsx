e5a2a084b40ad7bb3c08ec2b1fc721baa338e9f6
ayush-dev-17 <Ayushsc170197@gmail.com>
2025-06-11 07:30:24 +0530

    Update Tvdetails.jsx

--- src/components/Tvdetails.jsx
+++ src/components/Tvdetails.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { asyncloadtv, removetv } from "../store/actions/tvAction";
+import { asyncloadtv, removetv } from "../store/actions/TvAction";
 import {
   useNavigate,
   useParams,
