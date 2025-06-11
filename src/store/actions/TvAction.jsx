baa56c059195b1df0f82426121aa3e5b38b8ddf7
ayush-dev-17 <Ayushsc170197@gmail.com>
2025-06-11 07:40:34 +0530

    Update TvAction.jsx

--- src/store/actions/TvAction.jsx
+++ src/store/actions/TvAction.jsx
@@ -1,6 +1,6 @@
-export { removetv } from "../reducers/tvSlice";
 import axios from "../../utilis/Axios";
 import { loadtv } from "../reducers/tvSlice";
+export { removetv } from "../reducers/tvSlice";
 
 export const asyncloadtv = (id) => async (dispatch, getState) => {
   try {
