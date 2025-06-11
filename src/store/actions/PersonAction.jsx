d762e8a68edea650004ae28d6355d1a7ce90d747
ayush-dev-17 <Ayushsc170197@gmail.com>
2025-06-11 07:59:01 +0530

    Update PersonAction.jsx

--- src/store/actions/PersonAction.jsx
+++ src/store/actions/PersonAction.jsx
@@ -1,6 +1,6 @@
 import axios from "../../utilis/Axios"
-import { loadperson} from "../reducers/personSlice"
-export {removeperson} from "../reducers/personSlice"
+import { loadperson} from "../reducers/PersonSlice"
+export {removeperson} from "../reducers/PersonSlice"
 
 export const  asyncloadperson = (id)=> async(dispatch , getState)=>{
     try{
